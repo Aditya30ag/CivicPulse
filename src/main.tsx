176eac1337e9d7d9bb4602c601a223795b369b9a
Aditya Agrawal <adityaagrwal3005@gmail.com>
2026-07-14 13:06:47 +0530

    Revert "style"
    This reverts commit d4766479133a8c30f0075fbee10f377deb262ce5.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from './contexts/ThemeContext';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 );
